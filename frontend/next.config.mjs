bb69e1aee0f3fb9bdc1b2a57a3c5df0f4d1dd68a
Jakub Kunikowski <j.kunikowski.165@studms.ug.edu.pl>
2025-06-03 20:32:21 +0200

    update

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -5,3 +5,20 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'standalone',
+//   reactStrictMode: true,
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/api/:path*',
+//         destination: 'http://backend:3000/:path*',
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
